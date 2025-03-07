2c8986672f7b9d5eb19ee695c47a173b921e19b9
Casey Jenkins <casey.jenkins@protonmail.com>
2025-03-06 19:12:35 -0800

    Update discharge_capacity.r

--- discharge_capacity.r
+++ discharge_capacity.r
@@ -5,9 +5,9 @@ library(dplyr)
 library(ggplot2)
 library(data.table)
 
-# Read the discharge data from CSV file
+# Read the battery data from CSV file
 library(readxl)
-data <- read_excel("~/Downloads/test-battery-voltage.xlsx")
+data <- read_excel("~/Downloads/SP3_Initial capacity_10_16_2015/sp20.xlsx")
 
 str(data)
 
@@ -15,23 +15,32 @@ str(data)
 summary(data)
 
 # Plot voltage vs. time
-ggplot(data, aes(x = data$`Test_Time(s)...1`, y = data$`Current(A)`)) +
+ggplot(data, aes(x = data$`Test_Time(s)`, y = data$`Voltage(V)`)) +
   geom_line() +
   labs(title = "Battery Discharge",
-       x = "Time (s)", y = "Voltage (V)")
+       x = "Time (s)", y = "Voltage (V)") + theme_light()
 
 # Calculate discharge capacity
-discharge_capacity <- sum(data$voltage) * (data$time[2] - data$time[1])
+discharge_capacity <- sum(data$`Voltage(V)`) * (data$`Test_Time(s)`[2] - data$`Test_Time(s)`[1])
 cat("Discharge Capacity:", discharge_capacity, "Ah\n")
 
+discharge_capacity / 3600
+
 # Calculate average discharge voltage
-average_voltage <- mean(data$voltage)
+average_voltage <- mean(data$`Voltage(V)`)
 cat("Average Discharge Voltage:", average_voltage, "V\n")
 
 # Find the time at which voltage reaches a certain threshold
-threshold_voltage <- 1
-threshold_time <- data$time[which.min(abs(data$Act_I - threshold_voltage))]
+threshold_voltage <- 3.8
+threshold_time <- data$`Test_Time(s)`[which.min(abs(data$`Voltage(V)` - threshold_voltage))]
 cat("Time at Threshold Voltage:", threshold_time, "s\n")
 
 # Save plots and results
 ggsave("discharge_plot.png")
+
+
+# Plot voltage vs. time
+ggplot(data, aes(x = data$`Test_Time(s)`, y = data$`Discharge_Capacity(Ah)`)) +
+  geom_line() +
+  labs(title = "Capacity Discharge",
+       x = "Time (s)", y = "Voltage (V)") + theme_light()
